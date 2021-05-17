32229c4d28bc8bab3417fc832231686fbe48d9ec
jfpiovesa <78732629+jfpiovesa@users.noreply.github.com>
2021-05-17 09:11:30 -0300

    Initial commit

---
+++ Assets/AI.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using Panda;
+
+public class AI : MonoBehaviour
+{
+    public Transform player;// player
+    public Transform bulletSpawn;// onde vai spawn da bala 
+    public Slider healthBar;   // barra de vida
+    public GameObject bulletPrefab;// prefab bala
+
+    NavMeshAgent agent;
+    public Vector3 destination; // The movement destination.
+    public Vector3 target;      // The position to aim to.
+    float health = 100.0f;// vida 
+    float rotSpeed = 5.0f;// velocidade rotação
+
+    float visibleRange = 80.0f;//
+    float shotRange = 40.0f;//
+
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();//pegando componete navemeshe
+        agent.stoppingDistance = shotRange - 5; //for a little buffer
+        InvokeRepeating("UpdateHealth",5,0.5f);
+    }
+
+    void Update()
+    {
+        Vector3 healthBarPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        healthBar.value = (int)health;//falando que barra de vida é igual a vida
+        healthBar.transform.position = healthBarPos + new Vector3(0,60,0);
+    }
+
+    void UpdateHealth()
+    {
+       if(health < 100)// se a vida for menor que 100 adicionar mais vida
+        health ++;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "bullet")// se colider com a teg bullet tire  x de vida 
+        {
+            health -= 10;
+        }
+    }
+    [Task]
+    public void PickRandomDestination() 
+    {
+        Vector3 dest = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));// destino randomico  entre x valor a x valor 
+        agent.SetDestination(dest); // dando o destino do objeto, com o destino randomico
+        Task.current.Succeed();
+    }
+    [Task] 
+    public void MoveToDestination()// metodo de movimentação
+    {
+        if (Task.isInspected) 
+            Task.current.debugInfo = string.Format("t={0:0.00}", Time.time); //mostrando tempo de execução 
+
+        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+        { 
+            Task.current.Succeed();  
+        } 
+    }
+}
+
---
+++ Assets/Drive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour {
+
+	float speed = 20.0F;// velocidade
+    float rotationSpeed = 120.0F;// velocidade da  rotação 
+    public GameObject bulletPrefab;// prefab da bala
+    public Transform bulletSpawn;// local onde a  bala vai spawnar 
+
+    void Update() {
+        float translation = Input.GetAxis("Vertical") * speed;//movimentação de andar
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;// movimentação de rotação 
+        translation *= Time.deltaTime;// dando força de movimentação pra frente ou pra traz em soma com time da maquina
+        rotation *= Time.deltaTime;// dando força de movimentação pra rotacão esquerda ou direita em soma com time da maquina
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if(Input.GetKeyDown("space"))// se aperta  espaço
+        {
+            GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);// instaciando o prefab da bala ao aperta espaço
+            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*2000); // dando força e velocida quando a bala for instaciada
+        }
+    }
+}
